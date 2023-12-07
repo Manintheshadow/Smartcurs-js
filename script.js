9b63b218e31eeaf7dcb8ce294ec8c630d2bb7ac7
Manintheshadow <paliuga_20cm@yahoo.com>
2023-12-07 18:48:48 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let js = "Hi there";
-if (js === "boo") alert("Lock the door!")
+/* let js = "Hi there";
+if (js === "boo") alert("Lock the door!");
 
 console.log(40 + 8 * 2 + 16 + 53);
 let myName = Alex;
 let age = 35;
-console.log(myName)
+console.log(myName + " " + age);
 
-//camel casing
+//camel casing */
+
+let JavascriptIsFun = true;
+
+console.log(typeof true); 
+JavascriptIsFun = 23
+
+console.log(typeof JavascriptIsFun)
