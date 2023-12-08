634b31b31e2d946221492cd3047a1553e8f1e199
Manintheshadow <paliuga_20cm@yahoo.com>
2023-12-08 11:24:11 +0200

    update

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ console.log(myName + " " + age);
 let JavascriptIsFun = true;
 
 console.log(typeof true); 
-JavascriptIsFun = 23
+JavascriptIsFun = 23;
 
-console.log(typeof JavascriptIsFun)
+console.log(typeof JavascriptIsFun);
+JavascriptIsFun="Ion"
+console.log(typeof JavascriptIsFun);
+
+let age = 30;
+age = 31;
+console.log(age)
+
+const birthYear = 1988;
+console.log(birthYear);
+
+var myAge = 35;
+console.log(myAge);
+
+age = 35;
+console.log(age);
